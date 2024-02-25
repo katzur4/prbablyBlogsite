e228b45081fa47ec6258f3934c8721ec6f638fee
katzur4 <johnwesley.valentino@gmail.com>
2024-02-25 18:20:51 +0800

    creating test API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -14,4 +15,6 @@ app.listen(3000, () => {
 
     console.log('server runningasdf inasdf111port 3000asdaf!');
 
-}) ;
+});
+
+app.use('/api/user',userRoutes);
