c2ef1fe7ca51a6be9382d47b2c5fb12042e953c0
katzur4 <johnwesley.valentino@gmail.com>
2024-03-13 14:55:46 +0800

    create a post page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,8 @@ import Projects from './pages/Projects';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+import CreatePost from './pages/CreatePost';
 
 
 
@@ -25,7 +27,9 @@ export default function App() {
       <Route element={<PrivateRoute />}>
         <Route path="/dashboard" element={<Dashboard />} />
       </Route>
-
+      <Route element={<OnlyAdminPrivateRoute />}>
+        <Route path="/create-post" element={<CreatePost />} />
+      </Route>
     </Routes>
     <Footer />
    </BrowserRouter>
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -8,9 +8,10 @@ import 'react-circular-progressbar/dist/styles.css';
 import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess, signoutSuccess } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 export default function DashProfile() {
-    const {currentUser, error} = useSelector(state => state.user);
+    const {currentUser, error, loading} = useSelector(state => state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -185,9 +186,19 @@ export default function DashProfile() {
                 type="password" 
                 id="password" 
                 placeholder="Password" onChange={handleChange} />
-            <Button type="submit" gradientDuoTone='purpleToBlue' outline>
-                Update
+            <Button type="submit" gradientDuoTone='purpleToBlue' outline disabled={loading || imageFileUploading}>
+                {loading ? 'loading...' : 'Update'}
             </Button>
+            {currentUser.isAdmin && (
+                <Link to={'/create-post'}>
+                    <Button 
+                        type='button'
+                        gradientDuoTone='purpleToBlue'
+                        className="w-full">
+                    Create a Post
+                    </Button>
+                </Link>
+            )}
         </form>
         <div className="text-red-500 flex justify-between mt-5">
             <span onClick={()=>setShowModal(true)} className="cursor-pointer">Delete Account</span>
