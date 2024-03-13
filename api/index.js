34e71af5afc0c0f8a0f939565659480af03a8456
katzur4 <johnwesley.valentino@gmail.com>
2024-03-13 16:00:16 +0800

    create post api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-import authRoute from './routes/auth.route.js';
+import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 
@@ -26,7 +27,8 @@ app.listen(3000, () => {
 
 
 app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoute);
+app.use('/api/auth',authRoutes);
+app.use('/api/post',postRoutes)
 
 app.use(express.static(path.join(__dirname,'/client/dist')));
 
