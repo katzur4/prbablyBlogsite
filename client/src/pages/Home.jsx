becdf5f779c29ada9c5f9f36e6a7c13389fee233
katzur4 <johnwesley.valentino@gmail.com>
2024-03-24 14:53:00 +0800

    posting cringe 2

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='min-h-screen mt-15'>
-      <div className='flex max-w-3xl mx-auto flex-col'>
-        <div className='flex-3 text-lg text-blue-950 dark:text-blue-300 '>Posting Cringe</div>
+    <div className='h-screen flex justify-center items-center'>
+      <div className='flex justify-center items-center h-96 w-96 border border-gray-700'>
+        <h1 className='text-xl font-bold text-blue-950 dark:text-blue-300 '>Posting Cringe...</h1>
       </div>
     </div>
   )
